d92c329fcfcd89f6c75c2c3ce9b7436d6068de23
dongri727 <84567162+dongri727@users.noreply.github.com>
2021-07-24 15:31:40 +0900

    お絵かきページを全部捨てて、表紙をいろいろいじって、成果ゼロ

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -31,115 +31,114 @@ class _WelcomePageState extends State<WelcomePage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('さあ　はじめましょう',
-          style: Theme.of(context).textTheme.headline6),
-        ),
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:[
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: Image.network(
-                    'https://scontent-nrt1-1.xx.fbcdn.net/v/t1.18169-9/17522554_1096553430451122_1075037664490641253_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=730e14&_nc_ohc=K5TLhKJCQnsAX8PclqL&_nc_ht=scontent-nrt1-1.xx&oh=6c41f92f69514869b5958c7a2e8682d4&oe=60FA94C8',
-                    width: 250,
-                    height: 250,
-                    fit: BoxFit.cover,
-                  ),
+                appBar: AppBar(
+                  title: Text('さあ　はじめましょう',
+                  style: Theme.of(context).textTheme.headline6),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                child: Text(
-                  'わたしたちの町',
-                  style: Theme.of(context).textTheme.headline1),
-                ),
+                body: SafeArea(
+                      child:
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children:[
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(30),
+                                  child: Image.network(
+                                    'https://scontent-nrt1-1.xx.fbcdn.net/v/t1.18169-9/17760229_1103611766411955_1234258523540735361_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=sTxD19-trcQAX_Djfxm&tn=nw2CDFpCnAx5XB9B&_nc_ht=scontent-nrt1-1.xx&oh=c210abc6d537dbb3da108fd1e24ae0b1&oe=61221ABA',
+                                    width: 250,
+                                    height: 250,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                                child: Text(
+                                  'わたしたちの町',
+                                  style: Theme.of(context).textTheme.headline1),
+                                ),
 
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                child: Text(
-                  'ふらったー小学校',
-                    style: Theme.of(context).textTheme.headline2),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(500, 10, 500, 0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Expanded(
-                      child: Align(
-                        alignment: Alignment(0, 0),
-                        child: TextField(
-                          keyboardType: TextInputType.number,
-                          decoration: InputDecoration(
-                            border: UnderlineInputBorder(),
-                            labelText: 'ねん',
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                                child: Text(
+                                  'ふらったー小学校',
+                                    style: Theme.of(context).textTheme.headline2),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(500, 10, 500, 0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    Expanded(
+                                      child: Align(
+                                        alignment: Alignment(0, 0),
+                                        child: TextField(
+                                          keyboardType: TextInputType.number,
+                                          decoration: InputDecoration(
+                                            border: UnderlineInputBorder(),
+                                            labelText: 'ねん',
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: TextField(
+                                        keyboardType: TextInputType.number,
+                                        decoration: InputDecoration(
+                                          border: UnderlineInputBorder(),
+                                          labelText: 'くみ',
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: TextField(
+                                        keyboardType: TextInputType.number,
+                                        decoration: InputDecoration(
+                                          border: UnderlineInputBorder(),
+                                          labelText: 'ばん',
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.amber,
+                                  ),
+                                  child: Text('なまえをかきましょう',
+                                      style: Theme.of(context).textTheme.headline3),
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => Hiragana('なまえをかきましょう')
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
                       ),
-                    ),
-                    Expanded(
-                      child: TextField(
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'くみ',
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: TextField(
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'ばん',
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.amber,
-                  ),
-                  child: Text('なまえをかきましょう',
-                      style: Theme.of(context).textTheme.headline3),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Hiragana('なまえをかきましょう')
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-
-        bottomNavigationBar: BottomNavigationBar(
-          // 5
-            selectedItemColor:
-            Theme.of(context).textSelectionTheme.selectionColor,
-            // 10
-            currentIndex: _selectedIndex,
-            // 11
-            onTap: _onItemTapped,
-            // 6
-            items: <BottomNavigationBarItem>[
-              const BottomNavigationBarItem(
-                  icon: Icon(Icons.map), label: 'ちず'),
-              const BottomNavigationBarItem(
-                  icon: Icon(Icons.border_color), label: 'かく'),
-              const BottomNavigationBarItem(
-                  icon: Icon(Icons.auto_awesome_mosaic_sharp), label: 'みる'),
-            ]));
-
+                bottomNavigationBar: BottomNavigationBar(
+                  // 5
+                    selectedItemColor:
+                    Theme.of(context).textSelectionTheme.selectionColor,
+                    // 10
+                    currentIndex: _selectedIndex,
+                    // 11
+                    onTap: _onItemTapped,
+                    // 6
+                    items: <BottomNavigationBarItem>[
+                      const BottomNavigationBarItem(
+                          icon: Icon(Icons.map), label: 'ちず'),
+                      const BottomNavigationBarItem(
+                          icon: Icon(Icons.border_color), label: 'かく'),
+                      const BottomNavigationBarItem(
+                          icon: Icon(Icons.auto_awesome_mosaic_sharp), label: 'みる'),
+                    ]));
   }
 }
